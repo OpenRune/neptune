af9913c24404f55d405e578145b3aa52d1b52bd8
MatthewBishop <55411296+MatthewBishop@users.noreply.github.com>
2025-03-25 11:10:24 -0400

    use: kt 1.9 and java 11

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,7 @@ dependencyResolutionManagement {
 
 pluginManagement {
     plugins {
-        kotlin("jvm") version "2.1.0"
+        kotlin("jvm") version "1.9.0"
         id("org.jmailen.kotlinter") version "5.0.1"
     }
 }
