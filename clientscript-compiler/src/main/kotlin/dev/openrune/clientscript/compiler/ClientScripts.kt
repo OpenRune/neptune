39741ac5dcfbf56cda52c6be195d5952311f21e7
MatthewBishop <55411296+MatthewBishop@users.noreply.github.com>
2025-03-25 13:15:21 -0400

    feat: remove toml loading/clikt from compiler

--- clientscript-compiler/src/main/kotlin/dev/openrune/clientscript/compiler/ClientScripts.kt
+++ clientscript-compiler/src/main/kotlin/dev/openrune/clientscript/compiler/ClientScripts.kt
@@ -1,17 +1,6 @@
 package dev.openrune.clientscript.compiler
 
-import cc.ekblad.toml.decode
-import cc.ekblad.toml.tomlMapper
 import ch.qos.logback.classic.LoggerContext
-import com.github.ajalt.clikt.core.CliktCommand
-import com.github.ajalt.clikt.core.context
-import com.github.ajalt.clikt.core.main
-import com.github.ajalt.clikt.output.MordantHelpFormatter
-import com.github.ajalt.clikt.parameters.options.default
-import com.github.ajalt.clikt.parameters.options.flag
-import com.github.ajalt.clikt.parameters.options.option
-import com.github.ajalt.clikt.parameters.types.choice
-import com.github.ajalt.clikt.parameters.types.path
 import com.github.michaelbull.logging.InlineLogger
 import com.github.michaelbull.logging.Logger
 import com.google.gson.GsonBuilder
@@ -25,6 +14,7 @@ import me.filby.neptune.clientscript.compiler.command.debug.DumpCommandHandler
 import me.filby.neptune.clientscript.compiler.command.debug.ScriptCommandHandler
 import me.filby.neptune.clientscript.compiler.configuration.BinaryFileWriterConfig
 import me.filby.neptune.clientscript.compiler.configuration.ClientScriptCompilerConfig
+import me.filby.neptune.clientscript.compiler.configuration.ClientScriptWriterConfig
 import me.filby.neptune.clientscript.compiler.trigger.ClientTriggerType
 import me.filby.neptune.clientscript.compiler.type.DbColumnType
 import me.filby.neptune.clientscript.compiler.type.ParamType
@@ -49,7 +39,6 @@ import kotlin.io.path.Path
 import kotlin.io.path.absolute
 import kotlin.io.path.exists
 import kotlin.io.path.isDirectory
-import kotlin.io.path.notExists
 import kotlin.io.path.readLines
 import kotlin.io.path.useLines
 import kotlin.system.exitProcess
@@ -57,70 +46,67 @@ import kotlin.system.exitProcess
 private const val VERSION = "1.0.1-SNAPSHOT"
 private val logger = InlineLogger()
 
-class ClientScriptCommand : CliktCommand(name = "cs2") {
-    private val configPath by option("--config-path", help = "Path to the config file.")
-        .path(mustExist = true, canBeDir = false, mustBeReadable = true)
-        .default(Path("neptune.toml"))
-
-    private val print by option("--print", help = "Print the configuration and exit.")
-        .flag()
-
-    private val logLevelName by option("--log-level", help = "Set the log level.")
-        .choice("off", "error", "warn", "info", "debug", "trace", "all", ignoreCase = true)
-        .default("info")
-
-    private val version by option("--version", help = "Print the version of the compiler.")
-        .flag()
-
-    init {
-        context {
-            helpFormatter = { MordantHelpFormatter(it, showDefaultValues = true) }
-        }
-    }
-
-    override fun run() {
-        configureLogLevel(logLevelName)
-
-        if (version) {
-            echo("Neptune ClientScript 2 Compiler")
-            echo("Version $VERSION")
-            exitProcess(0)
-        }
-
-        val config = loadConfig(configPath)
-        if (print) {
-            val json = GsonBuilder()
-                .create()
-                .toJson(config)
-            println(json)
-            exitProcess(0)
-        }
-
-        val basePath = configPath.absolute().parent
-        val sourcePaths = config.sourcePaths.map { basePath.resolve(it) }
-        val symbolPaths = config.symbolPaths.map { basePath.resolve(it) }
-        val libraryPaths = config.libraryPaths.map { basePath.resolve(it) }
-        val (binaryWriterConfig) = config.writers
-
-        val mapper = SymbolMapper()
-        val writer = if (binaryWriterConfig != null) {
-            val outputPath = basePath.resolve(binaryWriterConfig.outputPath)
-            BinaryFileScriptWriter(outputPath, mapper)
+fun parseArgs(args: Array<String>): Map<String, String> {
+    val parsedArgs = mutableMapOf<String, String>()
+    var currentOption = ""
+    for (i in args.indices) {
+        if (args[i].startsWith("--")) {
+            currentOption = args[i].removePrefix("--")
         } else {
-            null
+            parsedArgs[currentOption] = args[i]
         }
-
-        // load commands and clientscript id mappings
-        loadSpecialSymbols(symbolPaths, mapper)
-
-        // setup compiler and execute it
-        val compiler = ClientScriptCompiler(sourcePaths, libraryPaths, writer, symbolPaths, mapper)
-        compiler.setup()
-        compiler.run()
     }
+    return parsedArgs
 }
 
-fun main(args: Array<String>) = ClientScriptCommand().main(args)
+
+fun main(args: Array<String>) {
+    val parsedArgs = parseArgs(args)
+
+    val configPath = parsedArgs["config-path"]?.let { Path(it) } ?: Path("neptune.json")
+    val print = parsedArgs.containsKey("print")
+    val logLevelName = parsedArgs["log-level"] ?: "info"
+    val version = parsedArgs.containsKey("version")
+
+    configureLogLevel(logLevelName)
+
+    if (version) {
+        println("Neptune ClientScript 2 Compiler")
+        println("Version $VERSION")
+        exitProcess(0)
+    }
+
+    val config = createConfig()
+    if (print) {
+        val json = GsonBuilder()
+            .create()
+            .toJson(config)
+        println(json)
+        exitProcess(0)
+    }
+
+    val basePath = configPath.absolute().parent
+    val sourcePaths = config.sourcePaths.map { basePath.resolve(it) }
+    val symbolPaths = config.symbolPaths.map { basePath.resolve(it) }
+    val libraryPaths = config.libraryPaths.map { basePath.resolve(it) }
+    val (binaryWriterConfig) = config.writers
+
+    val mapper = SymbolMapper()
+    val writer = if (binaryWriterConfig != null) {
+        val outputPath = basePath.resolve(binaryWriterConfig.outputPath)
+        BinaryFileScriptWriter(outputPath, mapper)
+    } else {
+        null
+    }
+
+    // load commands and clientscript id mappings
+    loadSpecialSymbols(symbolPaths, mapper)
+
+    // setup compiler and execute it
+    val compiler = ClientScriptCompiler(sourcePaths, libraryPaths, writer, symbolPaths, mapper)
+    compiler.setup()
+    compiler.run()
+}
 
 private fun configureLogLevel(levelName: String) {
     val context = LoggerFactory.getILoggerFactory() as LoggerContext
@@ -138,24 +124,14 @@ private fun configureLogLevel(levelName: String) {
     root.level = level
 }
 
-private fun loadConfig(configPath: Path): ClientScriptCompilerConfig {
-    if (configPath.notExists()) {
-        logger.error { "Unable to locate configuration file: $configPath." }
-        exitProcess(1)
-    }
-
-    val tomlMapper = tomlMapper {
-        mapping<ClientScriptCompilerConfig>(
-            "sources" to "sourcePaths",
-            "symbols" to "symbolPaths",
-            "libraries" to "libraryPaths",
-            "excludes" to "excludePaths",
-            "writer" to "writers",
-        )
-        mapping<BinaryFileWriterConfig>("output" to "outputPath")
-    }
-    logger.info { "Loading configuration from $configPath." }
-    return tomlMapper.decode<ClientScriptCompilerConfig>(configPath)
+private fun createConfig(): ClientScriptCompilerConfig {
+    return ClientScriptCompilerConfig(
+        sourcePaths = listOf("src/"),
+        symbolPaths = listOf("symbols/"),
+        libraryPaths = listOf("src/osrs/"),
+        excludePaths = listOf("pack/"),
+        writers = ClientScriptWriterConfig(BinaryFileWriterConfig(outputPath = "pack/cs2/"))
+    )
 }
 
 private fun loadSpecialSymbols(symbolsPaths: List<Path>, mapper: SymbolMapper) {
