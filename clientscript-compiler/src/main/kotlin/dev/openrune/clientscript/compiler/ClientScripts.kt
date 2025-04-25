6210b0f462848132bb56f58044e9edab4e81b70b
Mark7625 <72366279+Mark7625@users.noreply.github.com>
2025-04-25 23:09:17 +0100

    ClientScripts Packing

--- clientscript-compiler/src/main/kotlin/dev/openrune/clientscript/compiler/ClientScripts.kt
+++ clientscript-compiler/src/main/kotlin/dev/openrune/clientscript/compiler/ClientScripts.kt
@@ -1,111 +1,52 @@
 package dev.openrune.clientscript.compiler
 
+import cc.ekblad.toml.model.TomlValue
+import cc.ekblad.toml.serialization.from
+import cc.ekblad.toml.tomlMapper
 import ch.qos.logback.classic.LoggerContext
 import com.github.michaelbull.logging.InlineLogger
 import com.github.michaelbull.logging.Logger
-import com.google.gson.GsonBuilder
+import me.filby.neptune.clientscript.compiler.ClientScriptCompiler
 import me.filby.neptune.clientscript.compiler.SymbolMapper
-import me.filby.neptune.clientscript.compiler.command.DbFindCommandHandler
-import me.filby.neptune.clientscript.compiler.command.DbGetFieldCommandHandler
-import me.filby.neptune.clientscript.compiler.command.EnumCommandHandler
-import me.filby.neptune.clientscript.compiler.command.ParamCommandHandler
-import me.filby.neptune.clientscript.compiler.command.PlaceholderCommand
-import me.filby.neptune.clientscript.compiler.command.debug.DumpCommandHandler
-import me.filby.neptune.clientscript.compiler.command.debug.ScriptCommandHandler
 import me.filby.neptune.clientscript.compiler.configuration.BinaryFileWriterConfig
 import me.filby.neptune.clientscript.compiler.configuration.ClientScriptCompilerConfig
-import me.filby.neptune.clientscript.compiler.configuration.ClientScriptWriterConfig
-import me.filby.neptune.clientscript.compiler.trigger.ClientTriggerType
-import me.filby.neptune.clientscript.compiler.type.DbColumnType
-import me.filby.neptune.clientscript.compiler.type.ParamType
-import me.filby.neptune.clientscript.compiler.type.ScriptVarType
-import me.filby.neptune.clientscript.compiler.writer.BinaryFileScriptWriter
-import me.filby.neptune.runescript.compiler.ScriptCompiler
-import me.filby.neptune.runescript.compiler.configuration.SymbolLoader
-import me.filby.neptune.runescript.compiler.symbol.SymbolTable
-import me.filby.neptune.runescript.compiler.type.MetaType
-import me.filby.neptune.runescript.compiler.type.PrimitiveType
-import me.filby.neptune.runescript.compiler.type.TupleType
-import me.filby.neptune.runescript.compiler.type.Type
-import me.filby.neptune.runescript.compiler.type.wrapped.VarBitType
-import me.filby.neptune.runescript.compiler.type.wrapped.VarClanSettingsType
-import me.filby.neptune.runescript.compiler.type.wrapped.VarClanType
-import me.filby.neptune.runescript.compiler.type.wrapped.VarClientType
-import me.filby.neptune.runescript.compiler.type.wrapped.VarPlayerType
-import me.filby.neptune.runescript.compiler.writer.ScriptWriter
+import me.filby.neptune.clientscript.compiler.configuration.ClientScriptCompilerFeatureSet
 import org.slf4j.LoggerFactory
 import java.nio.file.Path
-import kotlin.io.path.Path
 import kotlin.io.path.absolute
 import kotlin.io.path.exists
-import kotlin.io.path.isDirectory
+import kotlin.io.path.notExists
 import kotlin.io.path.readLines
-import kotlin.io.path.useLines
 import kotlin.system.exitProcess
 
-private const val VERSION = "1.0.1-SNAPSHOT"
 private val logger = InlineLogger()
 
-fun parseArgs(args: Array<String>): Map<String, String> {
-    val parsedArgs = mutableMapOf<String, String>()
-    var currentOption = ""
-    for (i in args.indices) {
-        if (args[i].startsWith("--")) {
-            currentOption = args[i].removePrefix("--")
-        } else {
-            parsedArgs[currentOption] = args[i]
-        }
+object ClientScripts {
+
+    fun compileTask(configPath : Path, clientVersion : Int,clientScriptLogLevel: ClientScriptLogLevel = ClientScriptLogLevel.INFO): MutableList<ScriptEntry> {
+        configureLogLevel(clientScriptLogLevel.toString())
+
+        val config = loadConfig(configPath,clientVersion)
+
+        val basePath = configPath.absolute().parent
+        val sourcePaths = config.sourcePaths.map { basePath.resolve(it) }
+        val symbolPaths = config.symbolPaths.map { basePath.resolve(it) }
+        val libraryPaths = config.libraryPaths.map { basePath.resolve(it) }
+        val features = config.features
+
+        val mapper = SymbolMapper()
+        val writer = OpenRuneScriptWriter(mapper)
+
+        // load commands and clientscript id mappings
+        loadSpecialSymbols(symbolPaths, mapper)
+
+        // setup compiler and execute it
+        val compiler = ClientScriptCompiler(sourcePaths, libraryPaths, writer, features, symbolPaths, mapper)
+        compiler.setup()
+        compiler.run()
+
+        return writer.scripts
     }
-    return parsedArgs
-}
-
-
-fun main(args: Array<String>) {
-    val parsedArgs = parseArgs(args)
-
-    val configPath = parsedArgs["config-path"]?.let { Path(it) } ?: Path("neptune.json")
-    val print = parsedArgs.containsKey("print")
-    val logLevelName = parsedArgs["log-level"] ?: "info"
-    val version = parsedArgs.containsKey("version")
-
-    configureLogLevel(logLevelName)
-
-    if (version) {
-        println("Neptune ClientScript 2 Compiler")
-        println("Version $VERSION")
-        exitProcess(0)
-    }
-
-    val config = createConfig()
-    if (print) {
-        val json = GsonBuilder()
-            .create()
-            .toJson(config)
-        println(json)
-        exitProcess(0)
-    }
-
-    val basePath = configPath.absolute().parent
-    val sourcePaths = config.sourcePaths.map { basePath.resolve(it) }
-    val symbolPaths = config.symbolPaths.map { basePath.resolve(it) }
-    val libraryPaths = config.libraryPaths.map { basePath.resolve(it) }
-    val (binaryWriterConfig) = config.writers
-
-    val mapper = SymbolMapper()
-    val writer = if (binaryWriterConfig != null) {
-        val outputPath = basePath.resolve(binaryWriterConfig.outputPath)
-        BinaryFileScriptWriter(outputPath, mapper)
-    } else {
-        null
-    }
-
-    // load commands and clientscript id mappings
-    loadSpecialSymbols(symbolPaths, mapper)
-
-    // setup compiler and execute it
-    val compiler = ClientScriptCompiler(sourcePaths, libraryPaths, writer, symbolPaths, mapper)
-    compiler.setup()
-    compiler.run()
 }
 
 private fun configureLogLevel(levelName: String) {
@@ -124,14 +65,39 @@ private fun configureLogLevel(levelName: String) {
     root.level = level
 }
 
-private fun createConfig(): ClientScriptCompilerConfig {
-    return ClientScriptCompilerConfig(
-        sourcePaths = listOf("src/"),
-        symbolPaths = listOf("symbols/"),
-        libraryPaths = listOf("src/osrs/"),
-        excludePaths = listOf("pack/"),
-        writers = ClientScriptWriterConfig(BinaryFileWriterConfig(outputPath = "pack/cs2/"))
+private fun loadConfig(configPath: Path, clientVersion : Int): ClientScriptCompilerConfig {
+    if (configPath.notExists()) {
+        logger.error { "Unable to locate configuration file: $configPath." }
+        exitProcess(1)
+    }
+
+    val document = TomlValue.from(configPath)
+    val defaultFeatures = ClientScriptCompilerFeatureSet(
+        dbFindReturnsCount = clientVersion >= 228,
+        ccCreateAssertNewArg = clientVersion >= 230,
     )
+    val tomlMapper = tomlMapper {
+        // these defaults are required for
+        //  1) if features is not defined at all
+        //  2) if some features are defined
+        default(ClientScriptCompilerConfig(features = defaultFeatures))
+        default(defaultFeatures)
+
+        mapping<ClientScriptCompilerConfig>(
+            "sources" to "sourcePaths",
+            "symbols" to "symbolPaths",
+            "libraries" to "libraryPaths",
+            "excludes" to "excludePaths",
+            "writer" to "writers",
+        )
+        mapping<ClientScriptCompilerFeatureSet>(
+            "db_find_returns_count" to "dbFindReturnsCount",
+            "cc_create_optional_assert_new" to "ccCreateAssertNewArg",
+        )
+        mapping<BinaryFileWriterConfig>("output" to "outputPath")
+    }
+    logger.info { "Loading configuration from $configPath." }
+    return tomlMapper.decode<ClientScriptCompilerConfig>(document)
 }
 
 private fun loadSpecialSymbols(symbolsPaths: List<Path>, mapper: SymbolMapper) {
@@ -162,215 +128,6 @@ private fun loadSpecialSymbols(symbolsPaths: List<Path>, mapper: SymbolMapper) {
                 val id = split[0].toInt()
                 val name = split[1]
                 mapper.putScript(id, name)
-            }
-        }
-    }
-}
-
-class ClientScriptCompiler(
-    sourcePaths: List<Path>,
-    libraryPaths: List<Path>,
-    scriptWriter: ScriptWriter?,
-    private val symbolPaths: List<Path>,
-    private val mapper: SymbolMapper,
-) : ScriptCompiler(sourcePaths, libraryPaths, scriptWriter) {
-    fun setup() {
-        triggers.registerAll<ClientTriggerType>()
-
-        // register types
-        types.registerAll<ScriptVarType>()
-        types.changeOptions("long") {
-            allowDeclaration = false
-        }
-
-        // special types for commands
-        types.register("hook", MetaType.Hook(MetaType.Unit))
-        types.register("stathook", MetaType.Hook(ScriptVarType.STAT))
-        types.register("invhook", MetaType.Hook(ScriptVarType.INV))
-        types.register("varphook", MetaType.Hook(VarPlayerType(MetaType.Any)))
-        types.register("dbcolumn", DbColumnType(MetaType.Any))
-        types.register("clientopnpc", MetaType.Script(ClientTriggerType.CLIENTOPNPC, MetaType.Unit, MetaType.Unit))
-        types.register("clientoploc", MetaType.Script(ClientTriggerType.CLIENTOPLOC, MetaType.Unit, MetaType.Unit))
-        types.register("clientopobj", MetaType.Script(ClientTriggerType.CLIENTOPOBJ, MetaType.Unit, MetaType.Unit))
-        types.register(
-            "clientopplayer",
-            MetaType.Script(ClientTriggerType.CLIENTOPPLAYER, MetaType.Unit, MetaType.Unit),
-        )
-        types.register("clientoptile", MetaType.Script(ClientTriggerType.CLIENTOPTILE, MetaType.Unit, MetaType.Unit))
-
-        // allow assignment of namedobj to obj
-        types.addTypeChecker { left, right -> left == ScriptVarType.OBJ && right == ScriptVarType.NAMEDOBJ }
-
-        // treat varp as alias of varp<int>
-        types.addTypeChecker { left, right ->
-            (left is VarPlayerType && left.inner == PrimitiveType.INT && right == ScriptVarType.VARP) ||
-                (left == ScriptVarType.VARP && right is VarPlayerType && right.inner == PrimitiveType.INT)
-        }
-
-        // register the dynamic command handlers
-        addDynamicCommandHandler("enum", EnumCommandHandler())
-        addDynamicCommandHandler("oc_param", ParamCommandHandler(ScriptVarType.OBJ))
-        addDynamicCommandHandler("nc_param", ParamCommandHandler(ScriptVarType.NPC))
-        addDynamicCommandHandler("lc_param", ParamCommandHandler(ScriptVarType.LOC))
-        addDynamicCommandHandler("struct_param", ParamCommandHandler(ScriptVarType.STRUCT))
-        addDynamicCommandHandler("db_find", DbFindCommandHandler(false))
-        addDynamicCommandHandler("db_find_with_count", DbFindCommandHandler(true))
-        addDynamicCommandHandler("db_find_refine", DbFindCommandHandler(false))
-        addDynamicCommandHandler("db_find_refine_with_count", DbFindCommandHandler(true))
-        addDynamicCommandHandler("db_getfield", DbGetFieldCommandHandler())
-
-        addDynamicCommandHandler("event_opbase", PlaceholderCommand(PrimitiveType.STRING, "event_opbase"))
-        addDynamicCommandHandler("event_mousex", PlaceholderCommand(PrimitiveType.INT, Int.MIN_VALUE + 1))
-        addDynamicCommandHandler("event_mousey", PlaceholderCommand(PrimitiveType.INT, Int.MIN_VALUE + 2))
-        addDynamicCommandHandler("event_com", PlaceholderCommand(ScriptVarType.COMPONENT, Int.MIN_VALUE + 3))
-        addDynamicCommandHandler("event_op", PlaceholderCommand(PrimitiveType.INT, Int.MIN_VALUE + 4))
-        addDynamicCommandHandler("event_comsubid", PlaceholderCommand(PrimitiveType.INT, Int.MIN_VALUE + 5))
-        addDynamicCommandHandler("event_com2", PlaceholderCommand(ScriptVarType.COMPONENT, Int.MIN_VALUE + 6))
-        addDynamicCommandHandler("event_comsubid2", PlaceholderCommand(PrimitiveType.INT, Int.MIN_VALUE + 7))
-        addDynamicCommandHandler("event_keycode", PlaceholderCommand(PrimitiveType.INT, Int.MIN_VALUE + 8))
-        addDynamicCommandHandler("event_keychar", PlaceholderCommand(PrimitiveType.CHAR, Int.MIN_VALUE + 9))
-
-        addDynamicCommandHandler("dump", DumpCommandHandler())
-        addDynamicCommandHandler("script", ScriptCommandHandler())
-
-        // symbol loaders
-        addSymConstantLoaders()
-
-        addSymLoader("bugtemplate", ScriptVarType.BUG_TEMPLATE)
-        addSymLoader("graphic", ScriptVarType.GRAPHIC)
-        addSymLoader("fontmetrics", ScriptVarType.FONTMETRICS)
-        addSymLoader("stat", ScriptVarType.STAT)
-        addSymLoader("synth", ScriptVarType.SYNTH)
-        addSymLoader("locshape", ScriptVarType.LOC_SHAPE)
-        addSymLoader("model", ScriptVarType.MODEL)
-        addSymLoader("interface", ScriptVarType.INTERFACE)
-        addSymLoader("toplevelinterface", ScriptVarType.TOPLEVELINTERFACE)
-        addSymLoader("overlayinterface", ScriptVarType.OVERLAYINTERFACE)
-        addSymLoader("component", ScriptVarType.COMPONENT)
-        addSymLoader("category", ScriptVarType.CATEGORY)
-        addSymLoader("wma", ScriptVarType.MAPAREA)
-        addSymLoader("mapelement", ScriptVarType.MAPELEMENT)
-        addSymLoader("loc", ScriptVarType.LOC)
-        addSymLoader("npc", ScriptVarType.NPC)
-        addSymLoader("obj", ScriptVarType.NAMEDOBJ)
-        addSymLoader("inv", ScriptVarType.INV)
-        addSymLoader("enum", ScriptVarType.ENUM)
-        addSymLoader("struct", ScriptVarType.STRUCT)
-        addSymLoader("seq", ScriptVarType.SEQ)
-        addSymLoader("dbtable", ScriptVarType.DBTABLE)
-        addSymLoader("dbrow", ScriptVarType.DBROW)
-        addSymLoader("dbcolumn") { DbColumnType(it) }
-        addSymLoader("param") { ParamType(it) }
-        addSymLoader("varp") { VarPlayerType(it) }
-        addSymLoader("varc") { VarClientType(it) }
-        addSymLoader("varbit", VarBitType)
-        addSymLoader("varclan") { VarClanType(it) }
-        addSymLoader("varclansetting") { VarClanSettingsType(it) }
-        addSymLoader("stringvector", ScriptVarType.STRINGVECTOR)
-    }
-
-    /**
-     * Looks for `constant.sym` and all `sym` files in `/constant` and registers them
-     * with a [ConstantLoader].
-     */
-    private fun addSymConstantLoaders() {
-        for (symbolPath in symbolPaths) {
-            // look for {symbol_path}/constant.sym
-            val constantsFile = symbolPath.resolve("constant.sym")
-            if (constantsFile.exists()) {
-                addSymbolLoader(ConstantLoader(constantsFile))
-            }
-
-            // look for {symbol_path}/constant/**.sym
-            val constantDir = symbolPath.resolve("constant")
-            if (constantDir.exists() && constantDir.isDirectory()) {
-                val files = constantDir
-                    .toFile()
-                    .walkTopDown()
-                    .filter { it.isFile && it.extension == "sym" }
-                for (file in files) {
-                    addSymbolLoader(ConstantLoader(file.toPath()))
-                }
-            }
-        }
-    }
-
-    /**
-     * Helper for loading external symbols from `sym` files with a specific [type].
-     */
-    private fun addSymLoader(name: String, type: Type) {
-        addSymLoader(name) { type }
-    }
-
-    /**
-     * Helper for loading external symbols from `sym` files with subtypes.
-     */
-    private fun addSymLoader(name: String, typeSuppler: (subTypes: Type) -> Type) {
-        for (symbolPath in symbolPaths) {
-            // look for {symbol_path}/{name}.sym
-            val typeFile = symbolPath.resolve("$name.sym")
-            if (typeFile.exists()) {
-                addSymbolLoader(TsvSymbolLoader(mapper, typeFile, typeSuppler))
-            }
-
-            // look for {symbol_path}/{name}/**.sym
-            val typeDir = symbolPath.resolve(name)
-            if (typeDir.exists() && typeDir.isDirectory()) {
-                val files = typeDir
-                    .toFile()
-                    .walkTopDown()
-                    .filter { it.isFile && it.extension == "sym" }
-                for (file in files) {
-                    addSymbolLoader(TsvSymbolLoader(mapper, file.toPath(), typeSuppler))
-                }
-            }
-        }
-    }
-}
-
-class ConstantLoader(private val path: Path) : SymbolLoader {
-    override fun SymbolTable.load(compiler: ScriptCompiler) {
-        path.useLines {
-            for (line in it) {
-                val split = line.split('\t', limit = 2)
-                if (split.size != 2) {
-                    continue
-                }
-
-                addConstant(split[0], split[1])
-            }
-        }
-    }
-}
-
-class TsvSymbolLoader(
-    private val mapper: SymbolMapper,
-    private val path: Path,
-    private val typeSupplier: (subTypes: Type) -> Type,
-) : SymbolLoader {
-    constructor(mapper: SymbolMapper, path: Path, type: Type) : this(mapper, path, { type })
-
-    override fun SymbolTable.load(compiler: ScriptCompiler) {
-        path.useLines { lines ->
-            for (line in lines) {
-                val split = line.split('\t')
-                if (split.size < 2) {
-                    continue
-                }
-
-                val id = split[0].toInt()
-                val name = split[1]
-                val subTypes = if (split.size >= 3) {
-                    val typeSplit = split[2].split(',')
-                    val types = typeSplit.map { typeName -> compiler.types.find(typeName) }
-                    TupleType.fromList(types)
-                } else {
-                    MetaType.Unit
-                }
-                val type = typeSupplier(subTypes)
-
-                val symbol = addBasic(type, name)
-                mapper.putSymbol(id, symbol)
             }
         }
     }
---
+++ clientscript-compiler/src/main/kotlin/dev/openrune/clientscript/compiler/OpenRuneScriptWriter.kt
@@ -0,0 +1,31 @@
+package dev.openrune.clientscript.compiler
+
+import io.netty.buffer.ByteBuf
+import io.netty.buffer.ByteBufAllocator
+import me.filby.neptune.clientscript.compiler.writer.BinaryScriptWriter
+import me.filby.neptune.runescript.compiler.codegen.script.RuneScript
+import java.io.ByteArrayOutputStream
+
+data class ScriptEntry(
+    val id : Int,
+    val archiveName : String,
+    val bytes : ByteArray
+)
+
+/**
+ * An implementation of [BinaryScriptWriter] that writes the scripts to [scripts] .
+ */
+class OpenRuneScriptWriter(
+    idProvider: IdProvider,
+    allocator: ByteBufAllocator = ByteBufAllocator.DEFAULT,
+) : BinaryScriptWriter(idProvider, allocator) {
+
+    val scripts = emptyList<ScriptEntry>().toMutableList()
+
+    override fun outputScript(script: RuneScript, data: ByteBuf) {
+        val id = idProvider.get(script.symbol)
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        data.readBytes(byteArrayOutputStream, data.readableBytes())
+        scripts.add(ScriptEntry(id, script.fullName, byteArrayOutputStream.toByteArray()))
+    }
+}
